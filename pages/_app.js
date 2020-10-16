d35f462c99a28874c30ad9b2ae7f837269d7150e
erlingfn <erling.nesset@gmail.com>
2020-10-16 20:08:49 +0200

    Add tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles.css'
+import '../styles/globals.css'
+import '../styles/tailwind.css'
 
 export default function MyApp({Component, pageProps}) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import useSWR from 'swr'
-import Person from '../components/Person'
+import StretchGoals from '../components/StretchGoals'
 import styles from '../frontpage.module.css'
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Index() {
+
   return (
     <div className={styles.parent}>
-      <div className={styles.stream + " " + styles.pane}>Stream</div>
+      <div className={styles.stream + " " + styles.pane + ' text-lg'}><p className='text-center'>Hehe</p></div>
       <div className={styles.chat + " " + styles.pane}>Chat</div>
       <div className={styles.beer + " " + styles.pane}>Beer</div>
-      <div className={styles.stretch + " " + styles.pane}>Stretchgoals</div>
+      <div className={styles.stretch + " " + styles.pane}><StretchGoals/></div>
       <div className={styles.vipps + " " + styles.pane}>Vipps</div>
     </div>
-  )
+  );
 }
