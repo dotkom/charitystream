eab4006023a044fe1b315d83c29a4479a234fb6e
erlingfn <erling.nesset@gmail.com>
2020-10-16 18:57:50 +0200

    Add simple boxes to display grid

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 import useSWR from 'swr'
 import Person from '../components/Person'
-import styles from '../styles.module.css'
+import styles from '../frontpage.module.css'
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Index() {
   return (
     <div className={styles.parent}>
-      <div className={styles.stream}>Stream</div>
-      <div className={styles.chat}>Chat</div>
-      <div className={styles.beer}>Beer</div>
-      <div className={styles.stretch}>Stretchgoals</div>
-      <div className={styles.vipps}>Vipps</div>
+      <div className={styles.stream + " " + styles.pane}>Stream</div>
+      <div className={styles.chat + " " + styles.pane}>Chat</div>
+      <div className={styles.beer + " " + styles.pane}>Beer</div>
+      <div className={styles.stretch + " " + styles.pane}>Stretchgoals</div>
+      <div className={styles.vipps + " " + styles.pane}>Vipps</div>
     </div>
   )
 }
