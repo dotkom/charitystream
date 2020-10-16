1b62b9574048fe3fdefed9f419119e5e57a09d54
Ludvig Hozman <ludvig123@gmail.com>
2020-10-16 22:29:54 +0200

    Add video and slido

--- components/Stream.js
+++ components/Stream.js
@@ -12,26 +12,9 @@ const Stream = () => {
             allowfullscreen
           ></iframe>
         </div>
-        {/*
-        <div style={{ width: "100%" }}>
-          <iframe
-            src="https://www.youtube.com/live_chat?v=DdM2dUG7Iiw&embed_domain=localhost"
-            width="100%"
-            height="600"
-            frameBorder="0"
-          ></iframe>
-        </div>*/}
-        <div style={{ width: "100%" }}>
-          <iframe
-            src="https://app.sli.do/event/xf2tgxre"
-            height="100%"
-            width="100%"
-            frameborder="0"
-            style={{ "min-height": "560px" }}
-          ></iframe>
-        </div>
       </div>
     </div>
   );
 };
 
+export default Stream;
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,38 @@
-import useSWR from 'swr'
-import StretchGoals from '../components/StretchGoals'
-import SilentAuction from '../components/SilentAuction'
-import Vips from '../components/Vips'
-import styles from '../frontpage.module.css'
+import useSWR from "swr";
+import StretchGoals from "../components/StretchGoals";
+import SilentAuction from "../components/SilentAuction";
+import Vips from "../components/Vips";
+import styles from "../frontpage.module.css";
+import Stream from "../components/Stream";
+import Chat from "../components/Chat";
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Index() {
+  const { data, error } = useSWR("/api/state", fetcher);
 
-  const { data, error } = useSWR('/api/state', fetcher)
-
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
 
   return (
     <>
-    <div className={styles.parent + " h-screen"}>
-      <div className={styles.stream + " " + styles.pane + ' text-lg'}><p className='text-center'>Hehe</p></div>
-      <div className={styles.chat + " " + styles.pane}>Chat</div>
-      <div className={styles.beer + " " + styles.pane}><Vips items={data.vips} /></div>
-      <div className={styles.stretch + " " + styles.pane}><StretchGoals /></div>
-      <div className={styles.vipps + " " + styles.pane}><SilentAuction items={data.auctions} /></div>
-    </div>
+      <div className={styles.parent + " h-screen"}>
+        <div className={styles.stream + " " + styles.pane + " text-lg"}>
+          <Stream />
+        </div>
+        <div className={styles.chat + " " + styles.pane}>
+          <Chat />
+        </div>
+        <div className={styles.beer + " " + styles.pane}>
+          <Vips items={data.vips} />
+        </div>
+        <div className={styles.stretch + " " + styles.pane}>
+          <StretchGoals />
+        </div>
+        <div className={styles.vipps + " " + styles.pane}>
+          <SilentAuction items={data.auctions} />
+        </div>
+      </div>
     </>
   );
 }
