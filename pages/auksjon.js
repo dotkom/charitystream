b51e2a0b769df1d30462a040788dc89b59633303
Ludvig Hozman <ludvig123@gmail.com>
2020-10-17 15:30:53 +0200

    Navigation from auction page

--- pages/auksjon.js
+++ pages/auksjon.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
 import useSWR from "swr";
 import AuctionItems from "../components/AuctionItems";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
+
+const NavBar = () => (
+  <div className="w-full p-10">
+    <Link href="/">
+      <a class="inline-block border border-blue-500 rounded text-xl py-1 px-3 bg-blue-500 text-white">
+        Tilbake til hovedsiden
+      </a>
+    </Link>
+  </div>
+);
 
 export default function AuctionItemsPage() {
   const { data, error } = useSWR("/api/state", fetcher, {
@@ -15,6 +26,7 @@ export default function AuctionItemsPage() {
 
   return (
     <div>
+      <NavBar />
       <AuctionItems items={data.auctions} />
     </div>
   );
