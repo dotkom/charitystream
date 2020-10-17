5cdb8763bc3a2c957b37e7aa6dd5c4d64d3839ad
Ludvig Hozman <ludvig123@gmail.com>
2020-10-17 17:57:49 +0200

    Add cors

--- pages/api/vipps.js
+++ pages/api/vipps.js
@@ -1,9 +1,28 @@
 import mongoose from "mongoose";
+import Cors from "cors";
 import { Vipps } from "../../models/schema.js";
 import { url } from "./state";
 
+const cors = Cors();
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 export default async function handler(req, res) {
   const { method, headers } = req;
+
+  await runMiddleware(req, res, cors);
 
   mongoose.connect(url, {
     useNewUrlParser: true,
