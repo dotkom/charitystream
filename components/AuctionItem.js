94bc0ae206c797f837105111556d4b2201041c03
William Andersson <wandersson.98@gmail.com>
2021-03-17 22:57:45 +0100

    more prettifying, but not super pretty:)))

---
+++ components/AuctionItem.js
@@ -0,0 +1,15 @@
+const AuctionItem = (props) => {
+  return (
+    <div
+      className="w-48 rounded overflow-hidden shadow-lg m-5 text-center px-3 py-2 cursor-pointer hover:bg-gray-600 bg-gray-800"
+      onClick={props.onClick}
+      style={{ background: "#f9b759" }}
+    >
+      <div className="text-black font-bold text-2xl mb-2">{props.price},-</div>
+      <hr />
+      <p className="text-black text-base mt-4 mb-4">{props.description}</p>
+    </div>
+  );
+};
+
+export default AuctionItem;
